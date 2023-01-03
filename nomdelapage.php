d8ceaea123fc8c796511d344d401583719a279e7
Raphael-Hernandez <raphaelhrndpro@gmail.com>
2023-01-03 13:29:19 +0100

    etape 2

---
+++ nomdelapage.php
@@ -0,0 +1,35 @@
+
+<?php
+include 'header.php';
+?>
+
+<div class="starter-template text-center py-5 px-3">
+    <h1>Bootstrap 5 Starter Template</h1>
+    <p class="lead">Gunakan dokumen ini sebagai cara cepat untuk memulai proyek baru.<br> Dokumen ini adalah contoh dalam menggunakan Bootstrap 5 Framework.</p>
+</div>
+<div class="row">
+    <div class="col-sm-6 mb-3 mb-sm-0">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include 'footer.php';
+?>
+
+
