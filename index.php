c246bb8e05933ad7971549d85433e2c3510a1064
Raphael-Hernandez <raphaelhrndpro@gmail.com>
2023-01-03 16:26:48 +0100

    etape 3

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$recherche_page = filter_input(INPUT_GET, 'page', FILTER_DEFAULT);
+
+switch ($recherche_page) {
+    case "header":
+        include("pages/header.php");
+        break;
+    case "footer":
+        include("pages/footer.php");
+        break;
+    case "nomdelapage":
+        include("pages/nomdelapage.php");
+        break;
+    default:
+        include("pages/notfound.php");
+}
